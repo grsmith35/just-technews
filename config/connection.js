bc7c09caa8fe7e54bb21dd0b544517cb1f6b488a
Riley Smith <gordonrileysmith@gmail.com>
2021-10-18 16:34:32 -0600

    finished up module

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require("sequelize");
 
-// require('dotenv').config();
+require('dotenv').config();
 
 // // create connection to our db
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -11,7 +11,7 @@ const { Sequelize } = require("sequelize");
 //   port: 3306
 // });
 
-// module.exports = sequelize;
+// 
 
 let sequelize;
 
@@ -24,3 +24,5 @@ if(process.env.JAWSDB_URL) {
     port: 3306
   })
 }
+
+module.exports = sequelize;
