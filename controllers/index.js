d989685e23c03650323839a46f2aa8bc0d4fa0ab
grsmith35 <gordonrileysmith@gmail.com>
2021-10-21 16:58:39 -0600

    working on auth

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const dashboardRoutes = require('./dashboard-routes');
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
