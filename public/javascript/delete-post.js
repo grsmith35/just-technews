f4d38c2a24fd1d3b8ccc460c8a8ca25bf6eee882
grsmith35 <gordonrileysmith@gmail.com>
2021-10-22 23:35:07 -0600

    adding edit and delete posts

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,10 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = event.id;
+
+    
+  
+  }
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
